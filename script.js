3e1d8605f37e4ff83f9d9a9df426e9e965df4feb
Sardor <sardoribnfarhod@gmail.com>
2023-05-28 14:59:26 +0500

    tabs

---
+++ script.js
@@ -0,0 +1,18 @@
+const btns = document.querySelectorAll(".tab-btn");
+const aboutSection = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+aboutSection.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    });
+  }
+  articles.forEach((article) => {
+    article.classList.remove("active");
+  });
+  const activeEl = document.getElementById(id);
+  activeEl.classList.add("active");
+});
